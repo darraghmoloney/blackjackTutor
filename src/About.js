ba36f74ae844ffe9d2fc3ef8c9b01110f8c8d727
Derry Bradley <derrybradley@live.ie>
2020-03-27 16:28:42 +0000

    Adding the basic header with nav bar + Footer

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+
+    render () {
+
+        return (
+            <div className="About">
+            Hello World This is the about section 
+        </div>
+        )
+    }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import HeaderBox from './HeaderBox';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    let links = [
+      { label: 'Home', link: '#home', active: true },
+      { label: 'About', link: '#about' },
+      { label: 'Tutor', link: '#tutor' },
+      { label: 'Quiz', link: '#quiz' },
+      { label: 'Contact Us', link: '#contact-us' },
+    ];
+
+    return (
+      <div className="app">
+        <HeaderBox links={links} />
+        
+      
+      <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
